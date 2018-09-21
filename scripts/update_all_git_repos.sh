b41ae2096f3167ba6ad26b6cc7b4db2c4896ccaf
fabian <fabian@fabians-MacBook-Pro.local>
2018-09-21 11:22:55 +0700

    added echo output

--- scripts/update_all_git_repos.sh
+++ scripts/update_all_git_repos.sh
@@ -14,14 +14,15 @@
 
 # store the current dir
 CUR_DIR=$(pwd)
+echo 
 
 # Let the person running the script know what's going on.
-echo "Pulling in latest changes for all repositories..."
+echo "***** Pulling in latest changes for all repositories..."
 
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -name ".git" | cut -c 3-); do
     echo "";
-    echo "$i";
+    echo "***** $i";
 
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
@@ -33,5 +34,5 @@ for i in $(find . -name ".git" | cut -c 3-); do
     # lets get back to the CUR_DIR
     cd $CUR_DIR
 done
-
-echo "Complete! All Repos up to date"
+echo
+echo "***** Complete! All Repos up to date"
